b18288f1d46f6eae546ee68a787cfe7314a6c24e
Cas Hoefman <cashoefman@gmail.com>
2025-05-14 00:36:36 -0400

    Initial Commit

---
+++ FrescoDemoApp/FrescoDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrescoDemoAppApp.swift
+//  FrescoDemoApp
+//
+//  Created by Cas Hoefman on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct FrescoDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
